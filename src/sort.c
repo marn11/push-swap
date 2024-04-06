83df01b3ca77540929b45365b383cb13a03900bf
Marin Benchellal <yourintv.84@gmail.com>
2024-04-06 07:37:26 +0000

    x

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: mbenchel <mbenchel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 14:53:15 by mbenchel          #+#    #+#             */
-/*   Updated: 2024/04/06 07:27:18 by mbenchel         ###   ########.fr       */
+/*   Updated: 2024/04/06 07:37:15 by mbenchel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,13 @@ void	ft_sort(t_stack **a, t_stack **b)
 	while (ft_lstsize(*a) > 3)
 	{
 		if ((*a)->index < p1)
-			pb(a, b, 1);
-		else
 		{
-			ra(a, 1);
-			break ;
-			printf("size : %d\n", ft_lstsize(*a));
-			if (ft_lstsize(*a) == 3)
-				break ;
+			pb(a, b, 1);
+			index_stack(b);
+			index_stack(a);
 		}
+		else
+			ra(a, 1);
 		if (ft_lstsize(*b) > 1 && ((*b)->index >= lastp1 && (*b)->index >= p2))
 			rb(b, 1);
 		if (ft_lstsize(*b) == p1)
