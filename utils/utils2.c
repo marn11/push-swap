508266f4394ad67840981e93bf1dc480e68dc829
marn11 <yourintv.84@gmail.com>
2024-04-22 17:11:09 +0000

    x

--- utils/utils2.c
+++ utils/utils2.c
@@ -17,12 +17,10 @@ long	ft_atol(const char *nptr)
 	int		i;
 	int		sign;
 	long	result;
-	int		digc;
 
 	i = 0;
 	sign = 1;
 	result = 0;
-	digc = 0;
 	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
